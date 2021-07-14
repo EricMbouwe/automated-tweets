df0fea5c3c9e8bf42a38d1c548be74ba7c29dea3
ericmbouwe <ericmbouwe@yahoo.fr>
2021-07-14 23:26:35 +0100

    add password reset edit template

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,4 +12,27 @@ class PasswordResetsController < ApplicationController
         end
         redirect_to root_path, notice: 'If an account with that email has been found, we have sent you a link to reset your password'
     end
+
+    #edit password form mail link
+    def edit
+        @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: 'Your token has expired. Please try again'
+    end
+    
+    def update 
+        @user = User.find_signed!(params[:token], purpose: 'password_reset')
+
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: 'Your password was reset successfully. Please sign in'
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
