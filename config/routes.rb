483e7be2888ac2bd8e2834c6b2711baa94c781f8
ericmbouwe <ericmbouwe@yahoo.fr>
2021-07-14 19:51:35 +0100

    add user password reset actions routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
 
   get 'password/reset', to: 'password_resets#new'
   post 'password/reset', to: 'password_resets#create'
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
 end
