de2755ae18c58b58f6b37255f245830e959f9bf0
ericmbouwe <ericmbouwe@yahoo.fr>
2021-07-14 16:59:12 +0100

    add Current class

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
 class MainController < ApplicationController
     def index
-        flash.now[:notice] = 'Logged in successfully'
-        flash.now[:alert] = 'Invalid credentials'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@
 Rails.application.routes.draw do
   root to: 'main#index'
   get 'about-us', to: 'about#index', as: 'about'
+
   get 'sign-up', to: 'registrations#new'
   post 'sign-up', to: 'registrations#create'
+
+  get 'password', to: 'passwords#edit', as: 'edit_password'
+  patch 'password', to: 'passwords#update'
+
+  get 'sign-in', to: 'sessions#new'
+  post 'sign-in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 end
