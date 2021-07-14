f23196121fd26996b49b13a0008b0a87c60eb0ae
ericmbouwe <ericmbouwe@yahoo.fr>
2021-07-14 18:36:18 +0100

    add form object syntax

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,21 @@
+class PasswordsController < ApplicationController
+    before_action :require_user_logged_in!
+
+    def edit
+        #@user = User.find_by(id: session[:user_id])
+    end
+
+    def update
+        if Current.user.update(password_params)
+            redirect_to root_path, notice: 'password updated'
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+end
