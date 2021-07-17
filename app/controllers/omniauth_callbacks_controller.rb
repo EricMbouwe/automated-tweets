149c4237417e40d85ff77482090b864a40152887
ericmbouwe <ericmbouwe@yahoo.fr>
2021-07-17 16:57:03 +0100

    update twitter accounts index layout

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,13 +3,16 @@ class OmniAuthCallbacksController < ApplicationController
         # render plain: 'success!'
         Rails.logger.info auth
         
-        Current.user.twitter_accounts.create(
+        twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+        twitter_account.update(    
             name: auth.info.name,
             username: auth.info.nickname,
             image: auth.info.image,
             token: auth.credentials.token,
             secret: auth.credentials.secret,
         )
+
+        redirect_to root_path, notice: 'Successfully connected your account'
     end
 
     def auth
---
+++ app/controllers/twitter_accounts_controller.rb
@@ -0,0 +1,7 @@
+class TwitterAccountsController < ApplicationController
+    before_action :require_user_logged_in!
+
+    def index
+        @twitter_accounts = Current.user.twitter_accounts
+    end
+end
